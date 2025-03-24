ef61146954188449ea935e0a35439f702924e890
David Stack <2013388+davidstackio@users.noreply.github.com>
2025-03-24 15:43:43 -0400

    feat: add dialog

---
+++ src/runtime/composables/dialog.ts
@@ -0,0 +1,31 @@
+import { useState } from '#app'
+
+// Types
+export interface Dialog {
+  show: boolean
+  title: string
+  message: string
+  action: {
+    function: () => Promise<void>
+    buttonText: string
+    buttonColor: string
+  }
+  closeButtonText: string
+}
+
+export const dialogInitialState = <Dialog>{
+  show: false,
+  title: '',
+  message: '',
+  action: {
+    function: () => new Promise<void>(resolve => resolve()),
+    buttonText: '',
+    buttonColor: '',
+  },
+  closeButtonText: 'Cancel',
+}
+
+export const useDialog = () =>
+  useState('dialog', () => {
+    return dialogInitialState
+  })
