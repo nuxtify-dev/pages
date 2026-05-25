c8896444b4c1c0798b0f7c98c2a78d4133f47b18
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-05-25 18:02:28 -0400

    playground: add module welcome section

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -10,5 +10,18 @@ useSeoMeta({
 </script>
 
 <template>
-  <DemoCore class="py-8" />
+  <div>
+    <v-container class="py-8">
+      <v-row>
+        <v-col cols="12">
+          <h1>Welcome to the {{ nuxtifyConfig.brand?.name }} playground!</h1>
+          <p class="text-body-1 text-medium-emphasis">
+            Explore the core functionalities and configuration of the <strong>{{ nuxtifyConfig.brand?.name }}</strong> module.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <DemoCore />
+  </div>
 </template>
