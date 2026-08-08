a9a9136fc398118056814d8acf2be0834a3eb1d6
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-08-08 14:13:40 -0400

    playground: optimize vite deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,6 +2,14 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
   compatibilityDate: '2026-01-15',
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ],
+    },
+  },
   nuxtifyPages: {
     verboseLogs: true,
   },
